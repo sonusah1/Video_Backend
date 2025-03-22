7f2b430e92a3b40b437f51c1e205eabbd609e85b
sonusah1 <roshansah2267@gmail.com>
2025-03-23 01:48:30 +0530

    add getwatchHistory endpoint

--- src/cotrollers/user.controller.js
+++ src/cotrollers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/users.models.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiRespose from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefereshToken = async(userId) =>{
@@ -301,6 +302,130 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiRespose(200,user,"coverImage updated Successfully"));
 });
 
-export  {registerUser,loginUser,logoutUser,refreshAccessToken,getCurrentUser,changeCurrentPassword
-    ,updateAccountDetail,updateUserCoverImage,updateUserAvatar
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
+    const {username}=User.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, " username is missing");
+    }
+
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username
+                }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{$size:"$subscribers"},
+                subscribedToCount:{$size:"$subscribedTo"},
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                },
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscriberCount:1,
+                subscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1
+                }
+        }
+
+    ]);
+
+    console.log(channel);
+
+    if (!channel?.length) {
+        throw new ApiError(404,"channel not found");
+    }
+
+    return res.
+    status(200)
+    .json(new ApiRespose(200,channel[0],"User channel fetched sucessfully"));
+    
+});
+
+const getWatchHistory = asyncHandler(async (req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user?._id)
+                }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        username:1,
+                                        avatar:1,
+                                        fullName:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res.status(200).json(new ApiRespose(200,user[0].watchHistory,"User watch history fetched sucessfully"))
+})
+
+
+export  {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    getCurrentUser,
+    changeCurrentPassword
+    ,updateAccountDetail,
+    updateUserCoverImage,
+    updateUserAvatar,
+    getWatchHistory
 };
